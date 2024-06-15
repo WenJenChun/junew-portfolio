d43d663f5c8324548b11a847eaf2062be5af376b
WenJenChun <daydreamsss@hotmail.com>
2024-06-15 14:14:29 +0800

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        jOrange: '#F28500'
+      },
+    },
   },
   plugins: [],
 }
