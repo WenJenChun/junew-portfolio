cb45d42ff199417ba6d8b0f72a705c18b8f1ff1e
WenJenChun <daydreamsss@hotmail.com>
2024-07-03 12:29:54 +0800

    add modal

--- main.js
+++ main.js
@@ -11,4 +11,27 @@ function getLanguages() {
 }
 getLanguages();
 
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('hidden');
+    modal.classList.add('flex');
+  }
 
+  function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.add('hidden');
+    modal.classList.remove('flex');
+  }
+
+const care168 = document.querySelector("#care168");
+const closeBtn = document.querySelector("#closeBtn");
+
+care168.addEventListener("click", function(e){
+    console.log(e.target)
+    console.log("care168 clicked");
+    openModal('modalCare168');
+});
+
+closeBtn.addEventListener("click", function(e){
+    if(e.target.attra){}
+});
